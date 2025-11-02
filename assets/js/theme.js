96d1b727fd9e8719510aad8de5251ce9ea0da62e
sdu <648991806@qq.com>
2025-11-02 15:34:24 +0800

    Update IROS attending info; Modified paper img size; Adjust theme-model setting

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -7,8 +7,10 @@ let toggleThemeSetting = () => {
     setThemeSetting("light");
   } else if (themeSetting == "light") {
     setThemeSetting("dark");
-  } else {
-    setThemeSetting("system");
+  } 
+  else {
+    // setThemeSetting("system");
+    setThemeSetting("light");
   }
 };
 
@@ -209,8 +211,9 @@ let transTheme = () => {
 // "system". Default is "system".
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "system";
+  if (themeSetting != "dark" && themeSetting != "light"){ // && themeSetting != "system") {
+    // themeSetting = "system";
+    themeSetting = "light";
   }
   return themeSetting;
 };
@@ -219,16 +222,17 @@ let determineThemeSetting = () => {
 // "system", the computed theme is determined based on the user's system preference.
 let determineComputedTheme = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
-    const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    } else {
-      return "light";
-    }
-  } else {
-    return themeSetting;
-  }
+  // if (themeSetting == "system") {
+  //   const userPref = window.matchMedia;
+  //   if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+  //     return "dark";
+  //   } else {
+  //     return "light";
+  //   }
+  // } else {
+  //   return themeSetting;
+  // }
+  return themeSetting;
 };
 
 let initTheme = () => {
@@ -246,7 +250,7 @@ let initTheme = () => {
   });
 
   // Add event listener to the system theme preference change.
-  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
-    applyTheme();
-  });
+  // window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
+  //   applyTheme();
+  // });
 };
